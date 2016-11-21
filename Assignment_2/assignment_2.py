2ffbaf44172b09ba21733efd085a626ddc876c42
William Eriksson <willeeh@hotmail.com>
2016-11-21 20:29:13 +0100

    Brute is working, but is ugly :)

--- Assignment_2/assignment_2.py
+++ Assignment_2/assignment_2.py
@@ -9,8 +9,8 @@ from operator import itemgetter
 class Example(QtGui.QWidget):
     pointList = []
     lineList = []
-    firstCircleP = None
-    secondCircleP = None
+    c_mid = None
+    c_radius = None
     wHeight = 800
     wWidth = 1000
 
@@ -78,77 +78,95 @@ class Example(QtGui.QWidget):
 
 
     def isOutsideThreeP(self, a, b, c):
-        x_c = abs(a[0] - b[0])
-        y_c = abs(a[1] - b[1])
+        x_c = (a[0] + b[0]) / 2
+        y_c = (a[1] + b[1]) / 2
         radius = math.sqrt( (a[0] - b[0])**2 + (a[1] - b[1])**2 ) / 2
-        dist =  math.sqrt( (x_c - c[0])**2 + (y_c - c[1])**2 ) / 2
+        dist =  math.sqrt( (x_c - c[0])**2 + (y_c - c[1])**2 )
 
         if dist > radius:
             return True
         return False
 
-    def isOutside(self, a, b, c, d):
-        adx = a[0] - d[0];
-        ady = a[1] - d[1];
-        bdx = b[0] - d[0];
-        bdy = b[1] - d[1];
-        cdx = c[0] - d[0];
-        cdy = c[1] - d[1];
-        abdet = adx*bdy - bdx*ady;
-        bcdet = bdx*cdy - cdx*bdy;
-        cadet = cdx*ady - adx*cdy;
-        alift = adx*adx + ady*ady;
-        blift = bdx*bdx + bdy*bdy;
-        clift = cdx*cdx + cdy*cdy;
-        sign = alift*bcdet + blift*cadet + clift*abdet;
-        if sign < 0:
+    def isOutside(self, x, y, r, d):
+        dist = math.sqrt((x - d[0])**2 + (y - d[1])**2)
+
+        if dist > r:
             return True
         return False
 
+
+    # def isOutside(self, a, b, c, d):
+    #     adx = a[0] - d[0];
+    #     ady = a[1] - d[1];
+    #     bdx = b[0] - d[0];
+    #     bdy = b[1] - d[1];
+    #     cdx = c[0] - d[0];
+    #     cdy = c[1] - d[1];
+    #     abdet = adx*bdy - bdx*ady;
+    #     bcdet = bdx*cdy - cdx*bdy;
+    #     cadet = cdx*ady - adx*cdy;
+    #     alift = adx*adx + ady*ady;
+    #     blift = bdx*bdx + bdy*bdy;
+    #     clift = cdx*cdx + cdy*cdy;
+    #     sign = alift*bcdet + blift*cadet + clift*abdet;
+    #     if sign < 0:
+    #         return True
+    #     return False
+
     def smallesCircleThreePoints(self, a, b, c):
-        ab = math.sqrt( (a[0] - b[0])**2 + (a[1] - b[1])**2 )
-        ac = math.sqrt( (a[0] - c[0])**2 + (a[1] - c[1])**2 )
-        bc = math.sqrt( (b[0] - c[0])**2 + (b[1] - c[1])**2 )
-        if not self.isOutsideThreeP(a, b, c) and ab <= ac and ab <= bc:
-            # print a
-            # print b
-            # print '\n'
-            return [a, b]
+        x1 = a[0]
+        x2 = b[0]
+        x3 = c[0]
+        y1 = a[1]
+        y2 = b[1]
+        y3 = c[1]
 
-        if not self.isOutsideThreeP(a, c, b) and ac <= ab and ac <= bc:
-            # print a
-            # print c
-            # print '\n'
-            return [a, c]
+        num_x = (x1**2 + y1**2) * (y2 - y3) + (x2**2 + y2**2) * (y3 - y1) + (x3**2 + y3**2) * (y1 - y2)
+        den_x = 2 * (x1 * (y2 - y3) - y1 * (x2 - x3) + x2 * y3 - x3 * y2)
 
-        # print b
-        # print c
-        # print '\n'
-        return [b, c]
+        if den_x != 0:
+            cent_x = num_x / den_x
+
+            num_y = (x1**2 + y1**2) * (x3 - x2) + (x2**2 + y2**2) * (x1 - x3) + (x3**2 + y3**2) * (x2 - x1)
+            cent_y = num_y / den_x
+
+            r = math.sqrt((cent_x - x1)**2 + (cent_y - y1)**2)
+            return (cent_x, cent_y, r)
+
+        return (-1, -1, 9000000)
+
+
+    def newCircleMidPoint(self, a, b):
+        c_x = (a[0] + b[0]) / 2
+        c_y = (a[1] + b[1]) / 2
+        return (c_x, c_y)
 
     def bruteHandler(self):
+        self.c_mid = None
+        self.c_radius = None
         c_min = self.wHeight + self.wWidth
-        point_a = None
-        point_b = None
+        c_mid = None
+        c_radius = None
 
         for a in self.pointList:
             for b in self.pointList:
                 if b == a:
                     continue
 
+                valid = True
                 c = math.sqrt( (a[0] - b[0])**2 + (a[1] - b[1])**2 ) / 2 # radius
                 for d in self.pointList:
                     if d == a or d == b:
                         continue
 
                     if self.isOutsideThreeP(a, b, d):
-                        print "OUTSIDE"
+                        valid = False
                         break
 
-                if c < c_min:
+                if c < c_min and valid == True:
                     c_min = c
-                    point_a = a
-                    point_b = b
+                    c_mid = self.newCircleMidPoint(a, b)
+                    c_radius = c
 
         for a in self.pointList:
             for b in self.pointList:
@@ -159,30 +177,29 @@ class Example(QtGui.QWidget):
                     if c == a or c == b:
                         continue
 
-                tempPoints = self.smallesCircleThreePoints(a, b, c)
-                t_first = tempPoints[0]
-                t_second = tempPoints[1]
-                C = math.sqrt( (t_first[0] - t_second[0])**2 + (t_first[1] - t_second[1])**2 ) / 2
+                    valid = True
+                    (cent_x, cent_y, C) = self.smallesCircleThreePoints(a, b, c)
+                    for d in self.pointList:
+                        if d == a or d == b or d == c:
+                            continue
 
-                for d in self.pointList:
-                    if d == a or d == b or d == c:
-                        continue
+                        # if self.isOutside(a, b, c, d):
+                        #     valid = False
+                        #     break
 
-                    if self.isOutside(a, b, c, d):
-                        print "OUTSIDE2"
-                        break
+                        if self.isOutside(cent_x, cent_y, C, d):
+                            valid = False
+                            break
 
-                if C < c_min: # The points are always equal...
-                    point_a = t_first
-                    point_b = t_second
-                    print point_a
-                    print point_b
-                    print '\n'
-                    c_min = C
+                    if C < c_min and valid == True:
+                        c_min = C
+                        c_mid = (cent_x, cent_y)
+                        c_radius = C
 
 
-        self.firstCircleP = point_a
-        self.secondCircleP = point_b
+
+        self.c_mid = c_mid
+        self.c_radius = c_radius
 
     def randomHandler(self):
         self.resetHandler()
@@ -192,8 +209,8 @@ class Example(QtGui.QWidget):
     def resetHandler(self):
         self.pointList = []
         self.lineList = []
-        self.firstCircleP = None
-        self.secondCircleP = None
+        self.c_mid = None
+        self.c_radius = None
         QtGui.QWidget.update(self)
 
     def readHandler(self):
@@ -244,19 +261,15 @@ class Example(QtGui.QWidget):
             qp.drawLine(self.lineList[0][0], self.lineList[0][1], self.lineList[-1][0], self.lineList[-1][1])
         QtGui.QWidget.update(self)
 
-    def drawCircle(self, gp):
-        if self.firstCircleP and self.secondCircleP:
-            gp.setPen(QtCore.Qt.red)
+    def drawCircle(self, qp):
+        if self.c_mid and self.c_radius:
+            qp.setPen(QtCore.Qt.red)
             size = self.size()
-            a = self.firstCircleP
-            b = self.secondCircleP
-            radius = math.sqrt( (a[0] - b[0])**2 + (a[1] - b[1])**2 ) / 2
-
-            x = abs(a[0] - b[0])
-            y = abs(a[1] - b[1])
+            x = self.c_mid[0]
+            y = self.c_mid[1]
 
             center = QtCore.QPoint(x, y)
-            gp.drawEllipse(center, radius, radius)
+            qp.drawEllipse(center, self.c_radius, self.c_radius)
             QtGui.QWidget.update(self)
 
     def mousePressEvent(self, event):
