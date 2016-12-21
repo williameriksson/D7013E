de82f0ee2deea4493fdce82d3ad6fe74c3f09fa2
William Eriksson <willeeh@hotmail.com>
2016-12-21 12:53:26 +0100

    Added a GUI and fixed an error in the tree builder

--- Project/intervalTree.py
+++ Project/intervalTree.py
@@ -1,4 +1,7 @@
+import sys, random
+from PyQt4 import QtGui, QtCore
 from operator import itemgetter
+
 class node():
     def __init__(self, xMid, iMidLeft, iMidRight, lNode = None, rNode = None):
         self.xMid = xMid
@@ -6,6 +9,63 @@ class node():
         self.rNode = rNode
         self.iMidLeft = iMidLeft
         self.iMidRight = iMidRight
+
+class Example(QtGui.QWidget):
+    intervalList = []
+    inIntervalList = []
+    offset = 50
+    width = 1000
+    height = 1000
+    x = -100
+
+    def __init__(self, root, intervals):
+        self.root = root
+        self.intervalList = intervals
+        super(Example, self).__init__()
+        QtGui.QWidget.__init__(self)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(100, 100, self.width, self.height)
+        self.setWindowTitle('Intervals')
+        self.show()
+
+    def paintEvent(self, e):
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        self.drawIntervals(qp)
+        qp.end()
+
+
+    def mousePressEvent(self, event):
+        x = event.pos().x()
+        self.x = x
+        # print x
+        # event.pos().y()
+        # print queryTreeFromPoint(self.root, x - self.offset, [])
+        self.inIntervalList = queryTreeFromPoint(self.root, self.x - self.offset, [])
+        QtGui.QWidget.update(self)
+
+    def drawIntervals(self, qp):
+        black_pen = QtGui.QPen(QtCore.Qt.black, 2, QtCore.Qt.SolidLine)
+        green_pen = QtGui.QPen(QtCore.Qt.green, 2, QtCore.Qt.SolidLine)
+        red_pen = QtGui.QPen(QtCore.Qt.red, 2, QtCore.Qt.SolidLine)
+        n = 0
+        # print self.inIntervalList
+        for (start, end) in self.intervalList:
+            if (start, end) in self.inIntervalList:
+                qp.setPen(green_pen)
+            else:
+                qp.setPen(black_pen)
+
+            qp.drawLine(start + self.offset, self.offset + n - 5, start + self.offset, self.offset + n + 5)
+            qp.drawLine(start + self.offset, self.offset + n, end + self.offset, self.offset + n)
+            qp.drawLine(end + self.offset, self.offset + n - 5, end + self.offset, self.offset + n + 5)
+            n += 10
+
+        qp.setPen(red_pen)
+        qp.drawLine(self.x, self.offset - 20, self.x, self.offset + n + 10)
+
 
 
 def makeTree(lst):
@@ -19,7 +79,7 @@ def makeTree(lst):
     iLeft = []
     iRight = []
     for (start, end) in lst:
-        if start <= xMid and end >= end:
+        if start <= xMid and end >= xMid:
             iMid.append((start, end))
         elif end < xMid:
             iLeft.append((start, end))
@@ -52,7 +112,24 @@ def queryTreeFromPoint(v, p, lst = []):
                 break
         return queryTreeFromPoint(v.rNode, p, lst)
 
+def randomIntervals(start, end, n):
+    random.seed()
+    startNumbers = [random.randint(start, end - 100) for k in range(n)]
+    intervals = []
+    for i in startNumbers:
+        intervals.append( (i, random.randint(i + 10, end)) )
+    return intervals
 
-root = makeTree([(1,8), (2,4), (3,9), (9,12), (10, 14), (16, 20)])
-# print root.xMid
-print queryTreeFromPoint(root, 3)
+def main():
+    # intervals = [(100,300), (50,200), (250,600), (650, 800), (500, 850), (400, 900)]
+    intervals = randomIntervals(0, 900, 40)
+    root = makeTree(intervals)
+    # print root.xMid
+    # print queryTreeFromPoint(root, 3)
+    app = QtGui.QApplication(sys.argv)
+    ex = Example(root, intervals)
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
